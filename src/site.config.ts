aa16a67adc987efd0b695f1b9064da04b2009977
dddwill8 <46814786+dddwill8@users.noreply.github.com>
2025-01-02 17:39:18 +0800

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,13 +14,13 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "描述一下这是什么",
+	description: "雨伞是倒划天空的船",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "zh-CN",
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "zh-CN",
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "起个标题吧",
+	title: "hi,你好啊",
 };
 
 // Used to generate links in both the Header & Footer.
